6d28aeae959832cdd11001b185ecd3a0926807c3
Zayed Belim <zayedbehlim@gmail.com>
2024-11-05 15:01:04 +0000

    add simple for project for future

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("thinkspace.zad.thinkspace.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"thinkspacezad/thinkspace/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
