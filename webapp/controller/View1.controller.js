9e9d8bd83259bfe8b877192e676f2c857507bffb
Zayed Belim <zayedbehlim@gmail.com>
2024-11-06 05:43:57 +0000

    do it by zaid

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,25 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+  "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
+  function (Controller) {
     "use strict";
 
     return Controller.extend("thinkspace.zad.thinkspace.controller.View1", {
-        onInit: function () {
-            $.getScript("https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js", () => {
-                $.getScript("https://unpkg.com/vanta/dist/vanta.birds.min.js", () => {
-                  VANTA.BIRDS({
-                    el: "#vanta-bg",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00
-                  });
-                });
-              });
-        }
+      onInit: function () {
+        $.getScript("https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js", () => {
+          $.getScript("https://unpkg.com/vanta/dist/vanta.birds.min.js", () => {
+            VANTA.BIRDS({
+              el: "#vanta-bg",
+              mouseControls: true,
+              touchControls: true,
+              gyroControls: false,
+              minHeight: 200.00,
+              minWidth: 200.00,
+              scale: 1.00,
+              scaleMobile: 1.00,
+              backgroundColor: 0x3775cd
+            });
+          });
+        });
+      }
     });
-});
+  });
